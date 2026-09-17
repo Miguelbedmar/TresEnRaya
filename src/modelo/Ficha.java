664b7112ac539916f6e785d58ed181e4f328ec29
Miguel <miguelbedmards@gmail.com>
2026-09-17 11:13:59 +0200

    Se ha terminado el modelo Tablero

--- src/modelo/Ficha.java
+++ src/modelo/Ficha.java
@@ -1,6 +1,6 @@
 package modelo;
 
-public enum Ficha {
+public enum Ficha  {
 	// ATRIBUTOS
 	X, O;
 	
--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -12,4 +12,39 @@ public class Tablero {
 	
 	//METODOS
 	
+	public boolean jugar(Ficha ficha){
+		return false;
+	}
+
+	public boolean  estarLleno(){
+		return false;
+	}
+	public boolean ganarFicha(Ficha ficha){
+		return false;
+	}
+	protected boolean ganaHorizontal(Ficha ficha){
+		return false;
+	}
+	protected boolean  ganaVertical(Ficha ficha){
+		return false;
+	}
+	protected  boolean ganaDiagonalDirecta(Ficha ficha){
+		return false;
+	}
+	protected boolean ganaDiagonalIndirecta(Ficha ficha){
+		return false;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "{" +
+			"}";
+	}
+
+	private Object valueOf(Ficha ficha){
+		return ficha;
+	}
 }
+
+	
