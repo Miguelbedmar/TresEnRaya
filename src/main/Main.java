e9e5c58d98d7322f815796072f31fa37cca1f268
Miguel <miguelbedmar@gmail.com>
2026-09-23 19:12:50 +0200

    Se ha termindo el proyecto

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,25 +4,30 @@ import controlador.Partida;
 public class Main {
 
 	public static void main(String[] args) {
-		pedidaDatos();
-		
-
-
-		
-	}
-
-
-
-	public static  void pedidaDatos(){
 		Scanner sc = new Scanner(System.in);
 		int fila=0;
 		int columna=0;
-
+		Partida partida = new Partida(3);
+	
+		while(!partida.terminada()){
+		System.out.println(partida.toString());
+		
 		System.out.println("Ingrese el numero de filas que deseas");
 		 fila=sc.nextInt();
 
 		
 		System.out.println("Ingrese el numero de columnas que deseas");
 		columna=sc.nextInt();
+		
+		partida.jugar(fila, columna);
+			
+		}
+
+		if(partida.ganador()==null){
+			System.out.println("TABLAS");
+		}else{
+			System.out.println("El ganador de la partida es :"+partida.ganador());
+		}
+
 	}
 }
